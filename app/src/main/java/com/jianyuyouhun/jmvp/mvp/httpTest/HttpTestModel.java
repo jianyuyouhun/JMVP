d6092c6c92a0609edbbd32a7560311356a729edb
王宇 <1837960253@qq.com>
2017-05-03 20:55:45 +0800

    httpTest修改

--- app/src/main/java/com/jianyuyouhun/jmvp/mvp/httpTest/HttpTestModel.java
+++ app/src/main/java/com/jianyuyouhun/jmvp/mvp/httpTest/HttpTestModel.java
@@ -16,14 +16,11 @@ import java.util.Map;
 
 public class HttpTestModel extends BaseJModelImpl {
 
-    private static final String testUrl = "https://test-api.guijk.com/registration/patient/list";
-    private static final String jsonContent = "{\"op_type\":3016,\"task_id\":\"\",\"pid\":5,\"sid\":\"1ef91c212e30e14bf125e9374262401f\",\"user_id\":1003087,\"c_type\":1,\"gender\":3,\"c_ver\":5008}";
     public void doHttpTest(final OnResultListener<String> listener) {
         Map<String, String> params = new HashMap<>();
-        params.put("json", jsonContent);
         JHttpClient client = new JHttpFactory.ClientBuilder()
-                .setUrl(testUrl)
-                .setMethod(JHttpRequest.METHOD_POST)
+                .setUrl("https://jianyuyouhun.com")
+                .setMethod(JHttpRequest.METHOD_GET)
                 .setParams(params)
                 .build();
         JHttpFactory.getInstance().execute(client, new OnResultListener<String>() {

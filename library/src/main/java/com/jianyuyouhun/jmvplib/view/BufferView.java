43d69c86b862f05cc8573979d67391d5ab314b83
wangyu <1837960253@qq.com>
2017-07-21 09:53:01 +0800

    BufferView更新

--- library/src/main/java/com/jianyuyouhun/jmvplib/view/BufferView.java
+++ library/src/main/java/com/jianyuyouhun/jmvplib/view/BufferView.java
@@ -1,6 +1,7 @@
 package com.jianyuyouhun.jmvplib.view;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
@@ -9,9 +10,16 @@ import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 
+import com.jianyuyouhun.jmvplib.R;
+
 /**
  * 自定义视图
  * 可实现双缓冲绘制
+ * 备注：Android设备一般屏幕刷新率固定在60fps，这是由硬件决定的。所以每次onDraw绘制间隔时间最小值为16.66ms。
+ * 如果你的每次刷新耗时已经超过了这个时间，那么在尽量优化所有绘制过程之后依然超过16.66ms/每次的情况下，可以开启双缓冲。
+ * 双缓冲的原理是将所有绘制操作转为内存cpu处理然后生成一个bitmap，最后由gpu绘制这一个bitmap，
+ * 而绘制bitmap本身是较为耗时的（相比于绘制文本、线条等），所以这点也是需要考虑的。
+ * 知道双缓冲是门技术，知道何时开启双缓冲是门艺术。
  * Created by wangyu on 2017/6/8.
  */
 
@@ -21,6 +29,7 @@ public abstract class BufferView extends View {
     private Paint clearCanvasPaint;//清屏画笔
     private Canvas mBufferCanvas;//自定义缓冲区画布
     private Bitmap mBufferBitmap;//自定义缓冲位图
+    private int bgColor;//背景颜色，同时也是清屏画笔的颜色
 
     protected int mViewWidth;//视图宽度
     protected int mViewHeight;//视图高度
@@ -37,8 +46,12 @@ public abstract class BufferView extends View {
 
     public BufferView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        TypedArray array = context.getTheme().obtainStyledAttributes(attrs, R.styleable.BufferView, defStyleAttr, 0);
+        bgColor = array.getColor(R.styleable.BufferView_mBackgroundColor, Color.WHITE);
+        array.recycle();
         mContext = context;
         isBufferDrawMode = initBufferMode();
+        setBackgroundColor(bgColor);
         initClearPaint();
     }
 
@@ -48,7 +61,7 @@ public abstract class BufferView extends View {
     private void initClearPaint() {
         clearCanvasPaint = new Paint();
         clearCanvasPaint.setAntiAlias(true);
-        clearCanvasPaint.setColor(Color.WHITE);
+        clearCanvasPaint.setColor(bgColor);
         clearCanvasPaint.setStyle(Paint.Style.FILL);
     }
 
@@ -84,7 +97,7 @@ public abstract class BufferView extends View {
     }
 
     /**
-     * 绘制视图
+     * 绘制视图,大量绘制需求的情况下可以开启双缓冲
      * @param canvas  画布，开启双缓冲模式下是bufferCanvas, 否则是canvas
      */
     protected abstract void onCustomDraw(Canvas canvas);

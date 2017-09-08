011c9054c8afd6c758796ece776c14cbae604677
wangyu <1837960253@qq.com>
2017-09-08 15:28:48 +0800

    ViewInjector改成远程仓库依赖

--- inject/src/main/java/com/jianyuyouhun/inject/ViewInjectException.java
+++
@@ -1,9 +0,0 @@
-package com.jianyuyouhun.inject;
-
-public class ViewInjectException extends RuntimeException {
-    private static final long serialVersionUID = -3957558668444768796L;
-
-    public ViewInjectException(String string) {
-        super(string);
-    }
-}

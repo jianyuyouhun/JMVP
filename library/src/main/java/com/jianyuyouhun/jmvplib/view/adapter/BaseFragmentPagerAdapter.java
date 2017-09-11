c97d77e87575bbf9e080100b23fbd81f3c1a494d
wangyu <1837960253@qq.com>
2017-09-11 15:25:58 +0800

    修改注释

--- library/src/main/java/com/jianyuyouhun/jmvplib/view/adapter/BaseFragmentPagerAdapter.java
+++ library/src/main/java/com/jianyuyouhun/jmvplib/view/adapter/BaseFragmentPagerAdapter.java
@@ -6,8 +6,6 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.view.ViewGroup;
 
-import com.jianyuyouhun.jmvplib.utils.Logger;
-
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
@@ -35,7 +33,7 @@ public abstract class BaseFragmentPagerAdapter extends FragmentPagerAdapter {
     /**
      * 获取当前fragment实例
      *
-     * @param position  指定位置
+     * @param position  指定位置，一般用于获取当前位置的fragment，其他的fragment可能已经被系统回收
      * @return 指定位置的fragment对象
      */
     @Nullable
@@ -43,20 +41,22 @@ public abstract class BaseFragmentPagerAdapter extends FragmentPagerAdapter {
         String name = "";
         try {
             Class cls = FragmentPagerAdapter.class;
-            Method[] methods = cls.getDeclaredMethods();
-            for (Method method : methods) {
-                if (method.getName().equals("makeFragmentName")) {
-                    if (!method.isAccessible()) {
-                        method.setAccessible(true);
-                    }
-                    name = (String) method.invoke(cls, viewGroup.getId(), position);
-                }
+            Method method = cls.getDeclaredMethod("makeFragmentName", int.class, long.class);
+            if (!method.isAccessible()) {
+                method.setAccessible(true);
             }
+            name = (String) method.invoke(cls, viewGroup.getId(), position);
         } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
             e.printStackTrace();
         } catch (IllegalAccessException e) {
             e.printStackTrace();
         }
-        return manager.findFragmentByTag(name);
+        Fragment fragment = manager.findFragmentByTag(name);
+        if (fragment == null) {
+            return null;
+        }
+        return fragment;
     }
 }
